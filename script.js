07f86b513da75a4d410d7a4325e6f16738406129
JoyP321 <joy@djp3.net>
2019-09-10 09:55:40 -0700

    add some script to change questions

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-
 $(document).ready(function(){	
+	$("#question").click(function(){
+		$("#q1").show();
+		
+		
+		
+		$("#question").click(function(){
+			$("#q1").hide();
+			$("#q2").show();
+			$("#question").click(function(){
+				$("#q2").hide();
+				$("#q3").show();
+				$("#question").click(function(){
+					$("#q3").hide();
+					$("#q4").show();
+				});
+			});
+		});
+	});
+	
+	
 	
 });
