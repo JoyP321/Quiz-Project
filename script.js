134a0f32e49f2aa8f7d79b675fcaf645d5694188
JoyP321 <joy@djp3.net>
2019-09-10 09:59:02 -0700

    start adding the answer showing

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){	
 	$("#question").click(function(){
 		$("#q1").show();
-		
-		
-		
+		$("#answer").click(function(){
+			$("#a1").show();
+		});
 		$("#question").click(function(){
+			
 			$("#q1").hide();
 			$("#q2").show();
+			$("#answer").click(function(){
+				$("#a2").show();
+			});
+			
 			$("#question").click(function(){
+				
 				$("#q2").hide();
 				$("#q3").show();
 				$("#question").click(function(){
